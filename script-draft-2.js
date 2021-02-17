7824142738b54e2a048e4dd88237153c6337b404
Liv <lopez.olivia17@outlook.com>
2021-02-17 00:47:46 -0600

    Initial HTML, CSS, and some edited JS

---
+++ script-draft-2.js
@@ -0,0 +1,48 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// various characters that will be used to create password | Computer needs to calculate its input choice 
+
+var criteria = {
+  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9],
+  specialCharacters: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+}
+
+var choices;
+var confirmNumber;
+var confirmSpecialCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+function generatePassword(){
+  // do stuff to generate a password
+  passwordLength = prompt ('How many characters would you like in your password? Please enter a number between 8 and 128!');
+  // If, else statement 
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert ('Invalid please try again');
+    } else {
+  // changed method from prompt to confirm because prompt is asking for input while confirm is simply requesting validation 
+  confirmNumber = confirm ('Would you like numbers in your password?');
+  confirmSpecialCharacter = confirm ('Would you like special characters in your password?');
+  confirmUppercase = confirm ('Would you like uppercase letters in your password?');
+  confirmLowercase = confirm('Would you like lowercase letters in your password?');
+  };
+  // if, else statement 
+  if (!confirmNumber || !confirmSpecialCharacter || !confirmUppercase || !confirmLowercase) {
+    alert ('Invalid please try again');
+  } 
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);  
