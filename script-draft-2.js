86172213763a107c5082ed19fd4c7efc316ee1cf
Liv <lopez.olivia17@outlook.com>
2021-02-17 19:50:42 -0600

    adjusted and consolidated JS file

--- script-draft-2.js
+++ script-draft-2.js
@@ -10,22 +10,20 @@ var criteria = {
   specialCharacters: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
 };
 
+var minLength = 8;
+var maxLength = 128;
+// empty array and string to hold generated items 
 var characterPool = [];
 var password = '';
 
-// var confirmNumber;
-// var confirmSpecialCharacter;
-// var confirmUppercase;
-// var confirmLowercase;
-
+// conditionals, adding to the character pool
 function generatePassword() {
-  // do stuff to generate a password
   passwordLength = prompt ('How many characters would you like in your password? Please enter a number between 8 and 128!');
   // If, else statement 
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < minLength || passwordLength > maxLength) {
     alert ('Invalid please try again');
+    console.log(passwordLength);
     } else {
-  // changed method from prompt to confirm because prompt is asking for input while confirm is simply requesting validation 
   confirmNumber = confirm ('Would you like numbers in your password?');
   confirmSpecialCharacter = confirm ('Would you like special characters in your password?');
   confirmUppercase = confirm ('Would you like uppercase letters in your password?');
@@ -34,39 +32,101 @@ function generatePassword() {
   // if even one confirmation is not confirmed the alert will be returned to the user 
   if (!confirmNumber || !confirmSpecialCharacter || !confirmUppercase || !confirmLowercase) {
     alert ('Invalid please try again');
+    // or
+  } else if (confirmNumber && confirmSpecialCharacter && confirmUppercase && confirmLowercase); 
+  console.log(confirmNumber);
+  console.log(confirmSpecialCharacter);
+  console.log(confirmUppercase);
+  console.log(confirmLowercase); 
+  {
+  for (i = 1; i <= passwordLength; i++) {
+  index = Math.floor(Math.random() * characterPool.push(criteria));
+  console.log(index)
+  }
+  }
 
-    // numbers --------------
-    // confirmNumber will never === criteria.numbers because they are different data types 
-  } else if (confirmNumber) { 
-    // How values are chosen 
-    for (i = 1; i <= passwordLength; i++) {
-    index = Math.floor(Math.random() * characterPool.push(criteria[2]));
-    console.log(characterPool);
-    }
-    // special characters ------------------
-  } else if (confirmSpecialCharacter) {  
-    for (i = 1; i <= passwordLength; i++) {
-    index = Math.floor(Math.random() * characterPool.push(criteria.specialCharacters));
-    // character = characterPool.specialCharacters[index];
-    console.log(characterPool);
-    }
-    // uppercase ---------------------------------------
-  } else if (confirmUppercase) {  
-    for (i = 1; i <= passwordLength; i++) {
-    index = Math.floor(Math.random() * characterPool.push(criteria.uppercase));
-    // character = characterPool.uppercase[index];
-    console.log(characterPool);
-    }
-    // lowercase -------------------------------
-  } else if (confirmLowercase) {  
-    for (i = 1; i <= passwordLength; i++) {
-    index = Math.floor(Math.random() * characterPool.push(criteria.lowercase));
-    // character = characterPool.lowercase[index];
-    console.log(characterPool);
-    }
-    console.log("password: ", password)
-  } return password; 
+  console.log("password: ", password)
+  return password; 
 }
+
+  // (confirmNumber) { 
+  //   console.log(confirmNumber);
+  //   }
+  //   // special characters ------------------
+  //   if (confirmSpecialCharacter) {
+  //   console.log(confirmSpecialCharacter);
+  //   }
+  //   // uppercase ---------------------------------------
+  //   if (confirmUppercase) {
+  //   console.log(confirmUppercase);
+  //   }
+  //   // lowercase -------------------------------
+  //   if (confirmLowercase) {
+  //   console.log(confirmLowercase);
+  //   }
+    // loop over the character pool x times, adding a random character from this pool to the new password
+    // for (i = 1; i <= passwordLength; i++) 
+    // get a random index between 0 and the length of the character pool
+    // index = Math.floor(Math.random() * characterPool.push(criteria));
+    // use ^ to get a random character from the array
+    // add ( concatenate ) it to the password variable
+
+   // after the loop, return the password
+//     console.log("password: ", password)
+//     return password; 
+// }
+
+
+// function generatePassword() {
+//   // do stuff to generate a password
+//   passwordLength = prompt ('How many characters would you like in your password? Please enter a number between 8 and 128!');
+//   // If, else statement 
+//     if (passwordLength < 8 || passwordLength > 128) {
+//     alert ('Invalid please try again');
+//     console.log(passwordLength)
+//     } else {
+//   // changed method from prompt to confirm because prompt is asking for input while confirm is simply requesting validation 
+//   confirmNumber = confirm ('Would you like numbers in your password?');
+//   confirmSpecialCharacter = confirm ('Would you like special characters in your password?');
+//   confirmUppercase = confirm ('Would you like uppercase letters in your password?');
+//   confirmLowercase = confirm('Would you like lowercase letters in your password?');
+//   };
+//   // if even one confirmation is not confirmed the alert will be returned to the user 
+//   if (!confirmNumber || !confirmSpecialCharacter || !confirmUppercase || !confirmLowercase) {
+//     alert ('Invalid please try again');
+
+//     // numbers --------------
+//     // confirmNumber will never === criteria.numbers because they are different data types 
+//   } else if (confirmNumber) { 
+//     // How values are chosen 
+//     for (i = 0; i <= passwordLength - 1; i++) {
+//     index = Math.floor(Math.random() * characterPool.push(criteria.numbers));
+//     console.log(characterPool);
+//     }
+//     // special characters ------------------
+//   } else if (confirmSpecialCharacter) {  
+//     for (i = 1; i <= passwordLength; i++) {
+//     index = Math.floor(Math.random() * characterPool.push(criteria.specialCharacters));
+//     // character = characterPool.specialCharacters[index];
+//     console.log(characterPool);
+//     }
+//     // uppercase ---------------------------------------
+//   } else if (confirmUppercase) {  
+//     for (i = 1; i <= passwordLength; i++) {
+//     index = Math.floor(Math.random() * characterPool.push(criteria.uppercase));
+//     // character = characterPool.uppercase[index];
+//     console.log(characterPool);
+//     }
+//     // lowercase -------------------------------
+//   } else if (confirmLowercase) {  
+//     for (i = 1; i <= passwordLength; i++) {
+//     index = Math.floor(Math.random() * characterPool.push(criteria.lowercase));
+//     // character = characterPool.lowercase[index];
+//     console.log(characterPool);
+//     }
+//     console.log("password: ", password)
+//   } return password; 
+// }
 
 // Write password to the #password input
 function writePassword() {
